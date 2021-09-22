b09d525d95f3c1bab1dcc9636eaabb5b2cea7391
Siva <byteshiva@users.noreply.github.com>
2021-09-22 23:15:18 +0530

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,13 @@
-$("#flipbook").turn({
-	width: 800,
-	height: 400,
-	autoCenter: true
+var flipbookEL = document.getElementById('flipbook');
+
+window.addEventListener('resize', function (e) {
+	flipbookEL.style.width = '';
+  flipbookEL.style.height = '';
+  $(flipbookEL).turn('size', flipbookEL.clientWidth, flipbookEL.clientHeight);
+});
+
+$(flipbookEL).turn({
+    autoCenter: true
 });
 
 
