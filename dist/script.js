61b632e4c9ca68eef55130dcb0d603b3889c6eb6
Siva <byteshiva@users.noreply.github.com>
2021-09-22 23:24:31 +0530

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -10,21 +10,3 @@ $(flipbookEL).turn({
     autoCenter: true
 });
 
-
-$("#pageFld").val($("#flipbook").turn("page"));
-
-$("#flipbook").bind("turned", function(event, page, view) {
-		$("#pageFld").val(page);
-});
-
-$("#pageFld").change(function() {
-		$("#flipbook").turn("page", $(this).val());
-});
-
-$("#prevBtn").click(function() {
-		$("#flipbook").turn("previous");
-});
-
-$("#nextBtn").click(function() {
-		$("#flipbook").turn("next");
-});
